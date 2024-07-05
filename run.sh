ecaedc5ac314aeb6f3fef15fe0ffe18fcce94dff
ajitirto <ajiwoke@gmail.com>
2024-07-05 15:05:09 +0700

    perf: how to run

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ npm install
 
 npm run start
 
+sudo docker build -t express:v1 . 
+
+sudo docker run --name express-container-v1  -p 3000:3000 -d express:v1
+
 ### curl localhost:3000  # just "hello world"
