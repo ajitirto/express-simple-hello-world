81fbe0ab8b817be459494b6c75d96c961e92ee41
ajitirto <ajiwoke@gmail.com>
2024-07-05 15:21:54 +0700

    feat: just run it

--- run.sh
+++ run.sh
@@ -9,3 +9,17 @@ sudo docker build -t express:v1 .
 sudo docker run --name express-container-v1  -p 3000:3000 -d express:v1
 
 ### curl localhost:3000  # just "hello world"
+
+
+## docker-compose.yml
+
+# version: '3.8'
+# services:
+#   app:
+#     build:
+#       context: .
+#       target: Dockerfile
+#     ports:
+#       - "3000:3000"  
+#     command: ["node", "index.js"]
+#     working_dir: /app
