80c5dac63283a58a189b4ef48f3b7a6edf48c94d
ajitirto <ajiwoke@gmail.com>
2024-07-05 14:39:52 +0700

    docs: :lipstick: show bird in route /

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ import express from 'express';
 
 const app = express();
 
+const birdAscii = `
+        ___
+     __/o   \\_            | Documentation : (package.json)
+     \\______  \\           | "dependencies": {
+            \\  \\          | "express": "^4.17.1"
+             \\  \\         | ""nodemon": "^3.1.4"
+     ______   \\  \\_       |}
+   /       \\__|    o\\_    |
+  |           \\__ \\   |
+   \\________        __/   |
+            \\_____/       |
+
+============================
+=>  curl localhost:3000/
+
+            `;
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send(`<pre>${birdAscii}</pre>`);
 });
 
 const port = process.env.PORT || 3000;
